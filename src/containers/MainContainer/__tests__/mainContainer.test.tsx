07d0f6a065ba455122d2e9e1f0f03286b23bb2e1
ilarinie <ilari.nieminen@outlook.com>
2019-01-27 17:39:04 +0200

    Fix tests.

--- src/containers/MainContainer/__tests__/mainContainer.test.tsx
+++ src/containers/MainContainer/__tests__/mainContainer.test.tsx
@@ -1,9 +1,12 @@
+import { ApolloClient, InMemoryCache } from 'apollo-boost';
 import React from 'react';
+import { ApolloProvider } from 'react-apollo';
 import ReactDOM from 'react-dom';
 import MainContainer from '..';
+import client from '../../../services/ApolloClient';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<MainContainer />, div);
+  ReactDOM.render(<ApolloProvider client={client}><MainContainer /></ApolloProvider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
